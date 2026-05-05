7ebd1453aec92e09c768598839c4f8487db5e6b9
dianabui1407 <dianahongnhung.bui@stud.fhgr.ch>
2026-05-05 13:46:24 +0200

    HTML Titeländerung

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("blub");
+async function loadData() {
+    const url = 'https://tarotapi.dev/api/v1/cards/random'; // mit korrekter API-URL ersetzen
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const data = await loadData();
+console.log(data);
